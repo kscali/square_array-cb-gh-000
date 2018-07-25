919d135c04e0a47bdf4c0c6271209721fbac2df6
Koy Saeteurn <kosatun@aol.com>
2018-07-25 19:46:22 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,3 @@
 def square_array(array)
-  counter = 0 
-  array.each do |num|
-    square_num = num[counter] * num[counter]
-    counter += 1 
-  end  
-  square_num
+  array.each {|num| num*num}
 end
